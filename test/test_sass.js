f001fa6d81c611e4490228be226099f081730352
Eric Dauenhauer <eric_yd@yahoo.com>
2016-06-20 19:12:37 -0700

    finished unit tests for all generate functions

--- test/test_sass.js
+++ test/test_sass.js
@@ -1,8 +1,11 @@
 var path = require('path');
+var fs = require('fs');
 var sassTrue = require('sass-true');
 
-var sassFile = path.join(__dirname, 'test-generate-mono.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+var files = fs.readdirSync(__dirname);
 
-sassFile = path.join(__dirname, 'test-generate-complementary.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+for (var i = 0; i < files.length; i++) {
+    if (path.extname(files[i]) == '.scss') {
+        sassTrue.runSass({file: path.join(__dirname, files[i])}, describe, it);
+    }
+}
