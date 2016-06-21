dd55c001c6b0dd9986f233c4c849a435148d17e5
Eric Dauenhauer <eric_yd@yahoo.com>
2016-06-21 14:18:48 -0700

    created hogan template to render and display demo colors

--- demo/demo.js
+++ demo/demo.js
@@ -1,29 +1,71 @@
 var hogan = require('hogan.js');
 var fs = require('fs');
 var path = require('path');
+var open = require('open');
+var sass = require('node-sass');
 
-var schemes = ['mono', 'complementary', 'triad', 'tetrad', 'analogic'];
-var schemesObj = [{'scheme':'mono'}, {'scheme':'complementary'}, {'scheme':'triad'}, {'scheme':'tetrad'}, {'scheme':'analogic'}];
-var variants = ['--lighter', '--lightest', '--darker', '--darkest'];
-var variantsObj = [{'variant': '--lighter'}, {'variant': '--lightest'}, {'variant': '--darker'}, {'variant': '--darkest'}, {'variant': ''}];
-var keys = ['$primary', '$complementary', '$secondary-a', '$secondary-b'];
-var keysObj = [{'key': '$primary'}, {'key': '$complementary'}, {'key': '$secondary-a'}, {'key': '$secondary-b'}];
-var variations = ['pastel', 'soft', 'light', 'hard', 'pale', 'none'];
-var index;
-var partials = {};
-var data;
-
-fs.readdir(path.join(__dirname, 'templates'), function(err, files) {
+// compile sass
+sass.render({
+    file: 'demo/demo.scss'
+}, function(err, result) {
     if (err) throw err;
-    for (var i = 0; i < files.length; i++) {
-        data = fs.readFileSync(path.join(__dirname, 'templates', files[i]));
-        if (path.basename(files[i]) == 'index.hogan') {
-            index = hogan.compile(data.toString());
-        } else {
-            partials[path.basename(files[i],'.hogan')] = hogan.compile(data.toString());
-        }
-    }
-    fs.writeFile(path.join(__dirname, 'index.html'),
-                index.render({'schemes': schemesObj, 'variation': 'none', 'keys': keysObj, 'variants': variantsObj}, partials),
-                function(err) {if (err) throw err});
+    fs.writeFile(path.join(__dirname, 'demo.css'), result.css.toString(), function(err) {if (err) throw err;});
 });
+
+// fill arrays with values for hogan template
+var schemes = ['mono', 'complementary', 'triad', 'tetrad', 'analogic'];
+schemes.forEach(function(value, index, arr) {arr[index] = {'scheme': value};});
+
+var variants = ['--lightest', '--lighter', '', '--darker', '--darkest'];
+variants.forEach(function(value, index, arr) {arr[index] = {'variant': value};});
+
+var colors = ['primary', 'complementary', 'secondary-a', 'secondary-b'];
+colors.forEach(function(value, index, arr) {arr[index] = {'color': value};});
+
+var variations = ['pastel', 'soft', 'light', 'hard', 'pale', 'none'];
+variations.forEach(function(value, index, arr) {arr[index] = {'variation': value};});
+
+var index;
+
+// compile hogan template and open index.html
+fs.readFile(path.join(__dirname, 'templates', 'index.hogan'), function(err, data) {
+    if (err) throw err;
+    // write rendered result to index.html
+    fs.writeFile(path.join(__dirname, 'index.html'),
+                hogan.compile(data.toString()).render({
+                    'schemes': schemes, 
+                    'variation': 'none', 
+                    'colors': colors, 
+                    'variants': variants,
+                    }),
+                function(err) {if (err) throw err});
+    
+    // open index.html in browser
+    open(path.join(__dirname, 'index.html'));
+});
+
+
+/* shouldn't be necessary unless more templates are added to ./templates */
+// var partials = {};
+// var data;
+// fs.readdir(path.join(__dirname, 'templates'), function(err, files) {
+//     if (err) throw err;
+//     for (var i = 0; i < files.length; i++) {
+//         data = fs.readFileSync(path.join(__dirname, 'templates', files[i]));
+//         if (path.basename(files[i]) == 'index.hogan') {
+//             index = hogan.compile(data.toString());
+//         } else {
+//             partials[path.basename(files[i],'.hogan')] = hogan.compile(data.toString());
+//         }
+//     }
+//     fs.writeFile(path.join(__dirname, 'index.html'),
+//                 index.render({
+//                     'schemes': schemes, 
+//                     'variation': 'none', 
+//                     'colors': colors, 
+//                     'variants': variants,
+//                     }, partials),
+//                 function(err) {if (err) throw err});
+//     open(path.join(__dirname, 'index.html'));
+// });
+
